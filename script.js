bc91a820788a6fafb0d0353207b2a9e4382160e9
Neto <delcides.c.c.neto@gmail.com>
2025-01-14 16:30:46 -0300

    Upload dos Arquivos inciais, base toda funcionando

---
+++ script.js
@@ -0,0 +1,5 @@
+function ShowElementeAndTransition() {
+    var Tag = window.document.querySelector('.sobre');
+    Tag.style.opacity = '1';  // Torna o elemento visível
+    Tag.style.right = '50px';  // Move para a posição original
+}
